958332bb9d0750b0e8335759c831c7b975377ebf
ElvisM89 <80644724+ElvisMasiya89@users.noreply.github.com>
2022-10-22 15:40:26 +0200

    Model Serializer
    Model Serializer

--- framework/watchlist_class_app/api/serializers.py
+++ framework/watchlist_class_app/api/serializers.py
@@ -2,33 +2,59 @@ from rest_framework import serializers
 from ..models import Movie
 
 
-def name_length(value):
-    if len(value) < 2:
-        raise serializers.ValidationError("Name length must be between 2 and 255 characters")
+# Model Serializer
 
+class MovieSerializer(serializers.ModelSerializer):
+    len_name = serializers.SerializerMethodField()  # Serializer Custom field
 
-class MovieSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(validators=[name_length])
-    description = serializers.CharField()
-    active = serializers.BooleanField()
+    class Meta:
+        model = Movie
+        fields = '__all__'
+        # fields = ['id', 'name', 'description']  #Can select the fields you want to serializer
+        # exclude = ['name']                      #Can exclude the fields you don't want to serialize
 
-    def create(self, validated_data):
-        return Movie.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.description = validated_data.get('description', instance.description)
-        instance.active = validated_data.get('active', instance.active)
-        instance.save()
-        return instance
+    def get_len_name(self, object):
+        return len(object.name)
 
     def validate(self, data):
         if data['name'] == data['description']:
             raise serializers.ValidationError('Movie name cannot be the same as description')
         return data
 
-    # def validate_name(self, value):
-    #     if len(value) < 2:
-    #         raise serializers.ValidationError("Movie name must be at least 2 characters")
-    #     return value
+    def validate_name(self, value):
+        if len(value) < 2:
+            raise serializers.ValidationError("Movie name must be at least 2 characters")
+        return value
+
+# Field Serializer
+
+# def name_length(value):
+#     if len(value) < 2:
+#         raise serializers.ValidationError("Name length must be between 2 and 255 characters")
+#
+#
+# class MovieSerializer(serializers.Serializer):
+#     id = serializers.IntegerField(read_only=True)
+#     name = serializers.CharField(validators=[name_length])
+#     description = serializers.CharField()
+#     active = serializers.BooleanField()
+#
+#     def create(self, validated_data):
+#         return Movie.objects.create(**validated_data)
+#
+#     def update(self, instance, validated_data):
+#         instance.name = validated_data.get('name', instance.name)
+#         instance.description = validated_data.get('description', instance.description)
+#         instance.active = validated_data.get('active', instance.active)
+#         instance.save()
+#         return instance
+#
+#     def validate(self, data):
+#         if data['name'] == data['description']:
+#             raise serializers.ValidationError('Movie name cannot be the same as description')
+#         return data
+#
+#     def validate_name(self, value):
+#         if len(value) < 2:
+#             raise serializers.ValidationError("Movie name must be at least 2 characters")
+#         return value
